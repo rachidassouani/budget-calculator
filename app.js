26eee08cb90275483bd833f71a7fd6b18c446906
rachidassouani <rachidassouani@gmail.com>
2020-10-11 00:46:53 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// query selectors for inputs
+const reasonInput = document.querySelector("#input-reason");
+const amountInput = document.querySelector("#input-amount");
+
+// query selectors for btns
+const cancelBtn = document.querySelector("#btn-cancel");
+const confirmBtn = document.querySelector("#btn-confirm");
+
+// query selectors for expenses list
+const listExpenses = document.querySelector("#expenses-list");
+
+// total expenses element
+const totalExpensesEl = document.querySelector("#total-expenses");
+
+let totalExpenses = 0;
+
+// whene confirm button clicked by user
+confirmBtn.addEventListener('click', () => {
+  const entredReason = reasonInput.value;
+  const entredAmount = amountInput.value;
+
+  if (entredReason.trim().length <=0 || entredAmount <= 0 || entredAmount.trim().length <= 0) {
+    presentAlert();
+    return;
+  }
+  const newItem = document.createElement('ion-item');
+  newItem.textContent = entredReason + ' : $' + entredAmount;
+  listExpenses.appendChild(newItem);
+
+
+  // for total expenses
+  totalExpenses += +entredAmount; 
+  totalExpensesEl.textContent = totalExpenses;
+
+  // reset fields
+  clear();
+});
+
+// clear method to reset fields
+function clear() {
+  reasonInput.value = '';
+  amountInput.value = '';
+}
+
+// whene cancel button clicked by user
+cancelBtn.addEventListener('click', clear);
+
+
+function presentAlert() {
+  const alert = document.createElement('ion-alert');
+  alert.header = 'Alert';
+  alert.message = 'Please enter valid reason and amount';
+  alert.buttons = ['Okay'];
+
+  document.body.appendChild(alert);
+  return alert.present();
+}
